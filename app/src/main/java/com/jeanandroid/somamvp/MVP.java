9cadbde0cd18c7e765fbeb6712b82fe0209f2854
Lucas Calado <lcaraujo4252@gmail.com>
2018-10-24 07:59:03 -0300

    esqueci da interface rs

--- app/src/main/java/com/jeanandroid/somamvp/MVP.java
+++ app/src/main/java/com/jeanandroid/somamvp/MVP.java
@@ -4,12 +4,15 @@ public interface MVP {
 
     interface ModelInterface{
         void somarValores( int valorUm, int valorDois );
+        void verificaValor(String valorUm, String valorDois);
     }
 
     interface PresenterInterface{
         void setView( MVP.ViewInterface view );
         void getSoma( String valorUm, String valorDois );
+        void verificaValor(String valorUm, String valorDois); 
         void callbackSoma(String resultado);
+        void callbackVerifica(String resutVerifica);
     }
 
     interface ViewInterface{
